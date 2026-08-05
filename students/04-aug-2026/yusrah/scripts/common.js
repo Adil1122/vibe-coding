eac52715fcdb7b2c41c3c6c94e51c7c46ed7f939
Yusrah rafiq <mslawyer619@gmail.com>
2026-08-05 16:58:08 +0500

    yusrah

--- students/04-aug-2026/yusrah/scripts/common.js
+++ students/04-aug-2026/yusrah/scripts/common.js
@@ -1,25 +1,40 @@
 let students = [];
 
-function addStudent() {
-    let student_name = document.getElementById('student_name').value;
+function addstudent() {
 
-    if(student_name == '') {
-        alert('student name must be filled')
-        return; 
-    }
+    let student_name = document.getElementById('student_name').value;
     let age = document.getElementById('age').value;
-    if(age<=16) {
-        alert('age must be greater than  16')
-       // return;
+    let email = document.getElementById('email').value;
+    let skills = document.querySelectorAll('input[name="skills"]:checked');
+     if(student_name == "") {
+        alert('student_name must be filled')
+        return ; 
     }
-    //let email = document.getElementById('email').value;
-    //if(email=='not valid'){
-       // alert('email should be valid')
-        //return;
-    //}
-    //let skills = document.getElementById('skill').value;
-    //if(skills==''){
-       // alert('Atleast one skill must be selected')
-       // return;
-    //}//
-//}
+    
+      if(age <=16) {
+        alert("age must be greater than 16.")
+        return;
+    }
+    
+     if(email==="" ||!email.includes("@")){
+        alert("email should be valid.")
+        return;
+    }
+    
+      if(skills.length==0){
+        alert("Atleast one skill must be selected.")
+        return;
+    }
+    
+    let student ={
+        name: student_name,
+        age: age,
+        email: email,
+        skills: skills
+    }
+}
+    students.push(student);
+     
+    function displayStudents() {
+
+    }
