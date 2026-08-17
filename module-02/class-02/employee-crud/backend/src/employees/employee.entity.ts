f527464b2698e83eee4a6f563c19ef7d09cb38cc
Adil1122 <adil7090@gmail.com>
2026-08-17 12:57:54 +0500

    today changes

---
+++ module-02/class-02/employee-crud/backend/src/employees/employee.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('employees')
+export class Employee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  age: number;
+
+  @Column({ type: 'date' })
+  dob: Date;
+}
